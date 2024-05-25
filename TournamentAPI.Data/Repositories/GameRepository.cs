09aac2c3fd206cd37ff9c601d186c0d62716e5a2
98emre <emre-98@live.se>
2024-05-25 19:40:49 +0200

    feat: added implentation for game repository

---
+++ TournamentAPI.Data/Repositories/GameRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TournamentAPI.Core.Entities;
+using TournamentAPI.Core.Repositories;
+using TournamentAPI.Data.Data;
+
+namespace TournamentAPI.Data.Repositories
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly TournamentAPIContext _context;
+
+        public GameRepository(TournamentAPIContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Game game) => _context.Add(game);
+
+        public async Task<bool> AnyAsync(int id) => await _context.Game.AnyAsync(g => g.Id == id);
+
+        public async Task<IEnumerable<Game>> GetAllAsync() => await _context.Game.ToListAsync();
+
+        public async Task<Game> GetAsync(int id) => await _context.Game.FirstOrDefaultAsync(g => g.Id == id);
+
+        public void Remove(Game game) => _context.Game.Remove(game);
+
+        public void Update(Game game) => _context.Game.Update(game);
+    }
+}
