abc76692ffee87f66c679052e6c1faae8672072e
98emre <emre-98@live.se>
2024-05-25 22:01:10 +0200

    feat: In dto classes I added required and length to the title property.

--- TournamentAPI.Core/Dto/GameDto.cs
+++ TournamentAPI.Core/Dto/GameDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace TournamentAPI.Core.Dto
     {
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100")]
         public string Title { get; set; }
         
         public DateTime Time { get; set; }
--- TournamentAPI.Core/Dto/TournamentDto.cs
+++ TournamentAPI.Core/Dto/TournamentDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,13 @@ namespace TournamentAPI.Core.Dto
     public class TournamentDto
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Title must be between 3 and 100")]
         public string Title { get; set; }
+        
         public DateTime StartDate { get; set; }
+        
         public DateTime EndDate => StartDate.AddMonths(3);
     }
 }
