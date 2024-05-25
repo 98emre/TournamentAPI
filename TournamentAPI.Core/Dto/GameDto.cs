aaf0ff6cb1e0da574086afb5165bc4fcb0117e22
98emre <emre-98@live.se>
2024-05-25 20:35:38 +0200

    feat: added dto classes to the project

---
+++ TournamentAPI.Core/Dto/GameDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentAPI.Core.Dto
+{
+    public class GameDto
+    {
+        public string Title { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ TournamentAPI.Core/Dto/TournamentDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TournamentAPI.Core.Dto
+{
+    public class TournamentDto
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate => StartDate.AddMonths(3);
+    }
+}
